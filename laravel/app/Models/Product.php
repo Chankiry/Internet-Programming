ce1599ba98be938e88df3d052330d187d80a012c
kiry <kongchankiry2002@gmail.com>
2025-03-07 00:35:31 +0700

    modified product model

--- laravel/app/Models/Product.php
+++ laravel/app/Models/Product.php
@@ -13,7 +13,7 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'category_id', 'pricing', 'description', 'images'];
 
     public function category(): BelongsTo //M:1
     {
