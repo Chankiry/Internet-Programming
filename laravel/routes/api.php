af2a5a4bd3d9cc017e7adbfb497f29fc53be5d3a
kiry <kongchankiry2002@gmail.com>
2025-03-07 08:38:59 +0700

    finish controller products

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,4 +17,12 @@ Route::controller(CategoryController::class)->prefix('categories')->group(functi
     Route::get('/{categoryId}', 'getCategory');
     Route::patch('/{categoryId}', 'updateCategory');
     Route::delete('/{categoryId}', 'deleteCategory');
+});
+
+Route::controller(ProductController::class)->prefix('products')->group(function() {
+    Route::get('/', 'getProducts');
+    Route::post('/', 'createProduct');
+    Route::get('/{productId}', 'getProduct');
+    Route::patch('/{productId}', 'updateProduct');
+    Route::delete('/{productId}', 'deleteProduct');
 });
